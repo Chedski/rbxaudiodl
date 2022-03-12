174c952907a2b2cc8f4ea938704aa4d7113a6b38
Chedski <chedski@laurusgames.com>
2022-03-12 15:54:42 -0500

    Fixed a license violation (oops)

--- loading-spinner-mod.js
+++ loading-spinner-mod.js
@@ -1,3 +1,26 @@
+/*
+Copyright (c) 2018 Ivan Gabriele
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 const cursor = require('ansi')(process.stdout);
 const readline = require('readline');
 
@@ -29,10 +52,6 @@ var loadingSpinner = (function() {
       index = (index < sequence.length - 1) ? index + 1 : 0;
       process.stdout.write(sequence[index]);
     };
-
-    // if (settings.doNotBlock) {
-    //   spinnerTimer.unref();
-    // }
   }
 
   function stop() {
